e1270245fe93dda04ccaaf52179aea3842c9b7b5
git-0r <rohitdec97@gmail.com>
2022-06-05 23:15:17 +0530

    feat deployed server on digitalocean

--- src/api/axios-config.js
+++ src/api/axios-config.js
@@ -1,5 +1,6 @@
-const axios = require('axios');
+const axios = require("axios");
 
-axios.defaults.baseURL = process.env.NODE_ENV === "development"
+axios.defaults.baseURL =
+  process.env.NODE_ENV === "development"
     ? "http://localhost:3003"
-    : "https://ecom-srvr.herokuapp.com";
+    : "https://ecomsrvr-srtky.ondigitalocean.app";
